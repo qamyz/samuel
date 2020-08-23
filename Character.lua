2d3814f06e8f3a505843a58a583f014132849b19
qamyz <66923417+qamyz@users.noreply.github.com>
2020-08-23 05:04:56 -0400

    [Update Log 1.1]: Added a loop to the script

--- Character.lua
+++ Character.lua
@@ -1,3 +1,5 @@
+while wait(0.89)
+--Just for a unachoring the players body <For a game>
 game.Players.LocalPlayer.Character.Head.Anchored = false
 game.Players.LocalPlayer.Character.Torso.Anchored = false
 game.Players.LocalPlayer.Character["Left Arm"].Anchored = false
@@ -5,3 +7,4 @@ game.Players.LocalPlayer.Character["Right Arm"].Anchored = false
 game.Players.LocalPlayer.Character["Left Leg"].Anchored = false
 game.Players.LocalPlayer.Character["Right Leg"].Anchored = false
 game.Players.LocalPlayer.Character["HumanoidRootPart"].Anchored = false
+end
