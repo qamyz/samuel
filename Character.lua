758fb474f2717a484dafa204fae554359a42e7ee
qamyz <66923417+qamyz@users.noreply.github.com>
2020-09-10 18:42:04 -0400

    Delete Character.lua

--- Character.lua
+++
@@ -1,9 +0,0 @@
-game:GetService"RunService".RenderStepped:Connect(function()
-game.Players.LocalPlayer.Character.Head.Anchored = false
-game.Players.LocalPlayer.Character.Torso.Anchored = false
-game.Players.LocalPlayer.Character["Left Arm"].Anchored = false
-game.Players.LocalPlayer.Character["Right Arm"].Anchored = false
-game.Players.LocalPlayer.Character["Left Leg"].Anchored = false
-game.Players.LocalPlayer.Character["Right Leg"].Anchored = false
-game.Players.LocalPlayer.Character["HumanoidRootPart"].Anchored = false
-end)
