40d5dbfc18ead389fa384482fae81f2499ff25ab
qamyz <66923417+qamyz@users.noreply.github.com>
2020-09-10 18:47:47 -0400

    Delete KillAura.lua

--- KillAura.lua
+++
@@ -1,6 +0,0 @@
-game:GetService("RunService").RenderStepped:Connect(function()
-    local ohString1 = "WeakPunch"
-    local ohInstance2 = workspace.Entities.coldheck.Stand.StandRarm
-    local ohNumber3 = 0.5
-game:GetService("ReplicatedStorage").CombatRemote:FireServer(ohString1, ohInstance2, ohNumber3)
-end)
